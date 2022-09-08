09513e6efe42c49c618edcbdb3f596bec1ac66d8
Óscar M <osmuogar@gmail.com>
2022-09-09 01:00:36 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/*
+
+*/
+
+/**
+ * @param {number} a first number.
+ * @param {number} b second number.
+ * @return {number} The sum of the two numbers.
+ */
+function sum(a: number, b: number): number {
+    console.log('Sm');
+    return a + b;
+}
+
+sum(1, 2);
